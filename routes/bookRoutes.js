38a33b03e7b3c3baccb488990c066d7b518493e9
Ali Shaikh <alishaikh@xoho.tech>
2018-08-06 18:41:43 +0500

    Using Async wait

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,69 +1,20 @@
 const express = require('express');
 const sql = require('mssql');
 const debug = require('debug')('app:bookRoutes');
+
 const bookRouter = express.Router();
 
 function router(nav) {
-  const books = [
-    {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    },
-    {
-      title: 'Les Misérables',
-      genre: 'Historical Fiction',
-      author: 'Victor Hugo',
-      read: false
-    },
-    {
-      title: 'The Time Machine',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: false
-    },
-    {
-      title: 'A Journey into the Center of the Earth',
-      genre: 'Science Fiction',
-      author: 'Jules Verne',
-      read: false
-    },
-    {
-      title: 'The Dark World',
-      genre: 'Fantasy',
-      author: 'Henry Kuttner',
-      read: false
-    },
-    {
-      title: 'The Wind in the Willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Grahame',
-      read: false
-    },
-    {
-      title: 'Life On The Mississippi',
-      genre: 'History',
-      author: 'Mark Twain',
-      read: false
-    },
-    {
-      title: 'Childhood',
-      genre: 'Biography',
-      author: 'Lev Nikolayevich Tolstoy',
-      read: false
-    }];
   bookRouter.get('/', (req, res) => {
-    const request = new sql.Request();
-    request.query('select * from Books')
-      .then((result) => {
-        debug(result);
-        res.render('Books/bookListView', {
-          title: 'Books Page - NodeJS App with Auth - By Ali Shaikh',
-          books: result.recordset,
-          nav
-        });
+    (async function query() {
+      const request = new sql.Request();
+      const result = await request.query('select * from Books');
+      res.render('Books/bookListView', {
+        title: 'Books Page - NodeJS App with Auth - By Ali Shaikh',
+        books: result.recordset,
+        nav
       });
+    }());
   });
   bookRouter.get('/:id', (req, res) => {
     const { id } = req.params;
